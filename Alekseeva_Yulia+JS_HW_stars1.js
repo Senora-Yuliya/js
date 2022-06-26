cfa838524f7c35a76e354dde2f5008661a80bb1f
YULIYA ALEKSEEVA <yuliy@Miss-Jill.local>
2022-06-26 17:23:38 +0300

    Add HW2 JS stars 1

---
+++ Alekseeva_Yulia+JS_HW_stars1.js
@@ -0,0 +1,111 @@
+  const age2 = 18
+  const age3 = 60
+  
+  const checkAge = function(age)
+ {
+    if (age  < age2) {
+      console.log("You don’t have access cause your age is " + age + " " + "It’s less then");
+    } else if (age >= age2 && age < age3) {
+      console.log ("Welcome!");
+    } else if (age> age3) {
+      console.log("Keep calm and look Culture channel");
+    } else {
+      console.log("Technical Work");
+    }
+    }
+  
+    checkAge(17)
+    checkAge(18)
+checkAge(60)
+checkAge(70)
+
+
+
+
+const age2 = 18
+  const age3 = 60
+  
+  const checkAge = function(age)
+ { age = Number(age)
+    console.log(age)
+    if (age  < age2) {
+      console.log("You don’t have access cause your age is " + age + " " + "It’s less then");
+    } else if (age >= age2 && age < age3) {
+      console.log ("Welcome!");
+    } else if (age> age3) {
+      console.log("Keep calm and look Culture channel");
+    } else {
+      console.log("Technical Work");
+    }
+    }
+
+
+
+const age1 = 10
+const age2 = 18
+const age3 = 60
+{ 
+  if (age && isNaN(age)) 
+    if (!isNaN(age)) {
+  if (age  < age2) {
+    console.log("You don’t have access cause your age is " + age + " " + "It’s less then");
+  } else if (age >= age2 && age < age3) {
+    console.log ("Welcome!");
+  } else if (age> age3) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical Work");
+  }
+  } else console.log("Error") 
+}
+  checkAge(17)
+  checkAge(18)
+checkAge(60)
+checkAge(70)
+
+
+
+const age1 = 10
+const age2 = 18
+const age3 = 60
+{ 
+  if (age && isNaN(age)) 
+    if (!isNaN(age)) {
+  if (age  < age2) {
+    console.log("You don’t have access cause your age is " + age + " " + "It’s less then");
+  } else if (age >= age2 && age < age3) {
+    console.log ("Welcome!");
+  } else if (age> age3) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical Work");
+  }
+  } else console.log("Error") 
+}
+
+
+
+  checkAge(17)
+  checkAge(18)
+checkAge(60)
+checkAge(70)
+
+const age1 = 10
+const age2 = 18
+const age3 = 60
+{ 
+  if (age && isNaN(age)) 
+    if (!isNaN(age)) {
+  if (age  < age2) {
+    alert("You don’t have access cause your age is " + age + " " + "It’s less then");
+  } else if (age >= age2 && age < age3) {
+    alert ("Welcome!");
+  } else if (age> age3) {
+    alert("Keep calm and look Culture channel");
+  } else {
+    alert("Technical Work");
+  }
+  } else console.log("Error") 
+}
+let (a) = promt ("Enter your age")
+checkAge(a)
